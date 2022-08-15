bf24eaf87c3fa9462ef667bd3fff38c5069acd7e
= <=>
2022-08-15 12:28:42 +0530

    testing jsx functionality in checkout form

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PayTypeSelector from 'PayTypeSelector';
 
-document.addEventListener('turbolinks:load', function() {
+document.addEventListener('DOMContentLoaded', function() {
   var element = document.getElementById("pay-type-component");
   ReactDOM.render(<PayTypeSelector />, element);
 });
