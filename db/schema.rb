c8fd47ec9727ae2b6dd63d3b7a90ef93d3e2a23d
= <=>
2022-08-17 17:43:12 +0530

    enabled, discount_price and permalink columns added to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_023829) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_120305) do
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.integer "status", default: 0, null: false
     t.string "message_id", null: false
@@ -81,6 +81,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_023829) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "enabled"
+    t.decimal "discount_price"
+    t.string "permalink"
   end
 
   create_table "support_requests", force: :cascade do |t|
