b79106aa06d774df95e09ef496460d1229e5261c
= <=>
2022-08-19 15:38:03 +0530

    combination of product_id and cart_id is validated to be unique

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,8 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart, optional: true
 
+  validates: :product_id, uniqueness: { scope: :cart_id, message: "One product can be added only once in the cart" }
+  
   def total_price
     product.price * quantity
   end
