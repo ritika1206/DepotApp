b57a7feea4cfec9948e877f08e37b8d4f1db7423
= <=>
2022-08-22 11:11:01 +0530

    removed explicitly provided 'default: nil' for :enabled as by default it will be nil

--- db/migrate/20220817120305_add_columns_to_products.rb
+++ db/migrate/20220817120305_add_columns_to_products.rb
@@ -1,6 +1,6 @@
 class AddColumnsToProducts < ActiveRecord::Migration[7.0]
   def change
-    add_column :products, :enabled, :boolean, default: nil
+    add_column :products, :enabled, :boolean
     add_column :products, :discount_price, :decimal
     add_column :products, :permalink, :string
   end
