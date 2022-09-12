de6af1de191a0b5ca4630d9cb99a6e87d88ee99a
= <=>
2022-09-12 17:48:39 +0530

    - Add hit counter at the footer of every page - Add action response time in request response headers - Add user IP address at footer of every page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   # This will be invoked beofre every action in controller
   before_action :authorize
+  before_action :increment_hit_counter
+  around_action :action_response_time
 
   # By creating this method protected, it wont be exposed as public route
   protected def authorize
@@ -10,4 +12,17 @@ class ApplicationController < ActionController::Base
       redirect_to login_url, notice: "Please log in"
     end
   end
+
+  private
+    def increment_hit_counter
+      @logged_in_user = User.find_by(id: session[:user_id])
+      @logged_in_user.increment! :hit_count
+    end
+
+    def action_response_time
+      t = Time.now
+      yield
+      @action_response_time = (Time.now - t)*1000
+      response.set_header 'Action_Response_Time', @action_response_time
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_080217) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_111948) do
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.integer "status", default: 0, null: false
     t.string "message_id", null: false
@@ -126,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_080217) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "role", default: "user"
+    t.integer "hit_count", default: 0
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
