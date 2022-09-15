66320a48ec9abbcac1053db261c2b01e4afddf67
= <=>
2022-09-15 16:02:03 +0530

    Send order placement  confirmation mail to user

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,21 +1,11 @@
 class OrderMailer < ApplicationMailer
-  default from: 'Sam Ruby <depot@example.com>'
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.received.subject
-  #
+  default from: 'admin@depot.com'
   def received
     @order = order
 
     mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.shipped.subject
-  #
+  
   def shipped
     @order = order
     
